bf111ed42506b27214a578842e823b137896ff7e
Petterluring <petter.gustafsson1998@gmail.com>
2025-09-25 19:06:20 +0200

    Finishing testing of PatternScore

--- kotlin/games/yahtzee/app/src/main/kotlin/com/pattern/PatternScore.kt
+++ kotlin/games/yahtzee/app/src/main/kotlin/com/pattern/PatternScore.kt
@@ -18,21 +18,27 @@ data class PatternScore(
     val yahtzee: Int,
 ) {
 
+    companion object {
+        private const val VALUE_ERROR: String = "Illegal value"
+    }
+
     init {
-        require(ones in 0..5)
-        require(twos in setOf(0, 2, 4, 6, 8, 10))
-        require(threes in setOf(0, 3, 6, 9, 12, 15))
-        require(fours in setOf(0, 4, 8, 12, 16, 20))
-        require(fives in setOf(0, 5, 10, 15, 20, 25))
-        require(sixes in setOf(0, 6, 12, 18, 24, 30))
-        require(pair in setOf(0, 2, 4, 6, 8, 10, 12))
-        require(twoPair in setOf(0, 6, 8, 10, 12, 14, 16, 18, 20, 22))
-        require(threeOfAKind in setOf(0, 3, 6, 9, 12, 15, 18))
-        require(fourOfAKind in setOf(0, 4, 8, 12, 16, 20, 24))
-        require(smallStraight in setOf(0, 15))
-        require(largeStraight in setOf(0, 20))
-        require(fullHouse in setOf(0, 7, 9, 11, 13, 15, 8, 12, 14, 16, 18, 17, 19, 21, 22, 24, 23, 27, 20, 26, 28))
-        require(chance in 0..6*5)
-        require(yahtzee in setOf(0, 50))
+        require(ones in 0..5) {VALUE_ERROR}
+        require(twos in setOf(0, 2, 4, 6, 8, 10)) {VALUE_ERROR}
+        require(threes in setOf(0, 3, 6, 9, 12, 15)) {VALUE_ERROR}
+        require(fours in setOf(0, 4, 8, 12, 16, 20)) {VALUE_ERROR}
+        require(fives in setOf(0, 5, 10, 15, 20, 25)) {VALUE_ERROR}
+        require(sixes in setOf(0, 6, 12, 18, 24, 30)) {VALUE_ERROR}
+        require(pair in setOf(0, 2, 4, 6, 8, 10, 12)) {VALUE_ERROR}
+        require(twoPair in setOf(0, 6, 8, 10, 12, 14, 16, 18, 20, 22)) {VALUE_ERROR}
+        require(threeOfAKind in setOf(0, 3, 6, 9, 12, 15, 18)) {VALUE_ERROR}
+        require(fourOfAKind in setOf(0, 4, 8, 12, 16, 20, 24)) {VALUE_ERROR}
+        require(smallStraight in setOf(0, 15)) {VALUE_ERROR}
+        require(largeStraight in setOf(0, 20)) {VALUE_ERROR}
+        require(fullHouse in setOf(
+            0, 7, 9, 11, 13, 15, 8, 12, 14, 16, 18, 17, 19,
+            21, 22, 24, 23, 27, 20, 26, 28)) {VALUE_ERROR}
+        require(chance in 0..6*5) {VALUE_ERROR}
+        require(yahtzee in setOf(0, 50)) {VALUE_ERROR}
     }
 }
--- kotlin/games/yahtzee/app/src/test/kotlin/com/pattern/PatternScoreTest.kt
+++ kotlin/games/yahtzee/app/src/test/kotlin/com/pattern/PatternScoreTest.kt
@@ -1,5 +1,6 @@
 package com.pattern
 
+import org.junit.jupiter.api.assertThrows
 import kotlin.test.Test
 import kotlin.test.assertEquals
 
@@ -36,14 +37,132 @@ class PatternScoreTest {
     }
 
     @Test
-    fun `can assigns pair correctly`() {
+    fun `can assign pairs, three and four of a kind correctly`() {
+        for (dieValue in 0..6) {
+            val scores = PatternScore(
+                0,
+                0,
+                0,
+                0,
+                0,
+                0,
+                dieValue * 2,
+                0,
+                dieValue * 3,
+                dieValue * 4,
+                0,
+                0,
+                0,
+                0,
+                0,
+            )
+            assertEquals(dieValue * 2, scores.pair)
+            assertEquals(dieValue * 3, scores.threeOfAKind)
+            assertEquals(dieValue * 4, scores.fourOfAKind)
+        }
+    }
 
+    @Test
+    fun `can assign two pairs correctly`() {
+        for (d1 in 1..6) {
+            for (d2 in d1 + 1..6) {
+                val scores = PatternScore(
+                    0,
+                    0,
+                    0,
+                    0,
+                    0,
+                    0,
+                    0,
+                    d1 * 2 + d2 *2,
+                    0,
+                    0,
+                    0,
+                    0,
+                    0,
+                    0,
+                    0,
+                )
+                assertEquals(d1 * 2 + d2 * 2, scores.twoPair)
+            }
+        }
+    }
+
+    @Test
+    fun `can assign large and small straight, and Yatzy correctly`() {
+        val scores = PatternScore(
+            0,
+            0,
+            0,
+            0,
+            0,
+            0,
+            0,
+            0,
+            0,
+            0,
+            15,
+            20,
+            0,
+            0,
+            50,
+        )
+        assertEquals(15, scores.smallStraight)
+        assertEquals(20, scores.largeStraight)
+        assertEquals(50, scores.yahtzee)
+    }
+
+    @Test
+    fun `can assign full house correctly`() {
+        for (d1 in 1..6) {
+            for (d2 in 1..6) {
+                if (d1 == d2) continue
+                val scores = PatternScore(
+                    0,
+                    0,
+                    0,
+                    0,
+                    0,
+                    0,
+                    0,
+                    0,
+                    0,
+                    0,
+                    0,
+                    0,
+                    d1 * 2 + d2 * 3,
+                    0,
+                    0,
+                )
+                assertEquals(d1 * 2 + d2 * 3, scores.fullHouse)
+            }
+        }
     }
 
     @Test
     fun `can identify invalid inputs`() {
-        for (value in listOf(-1, 10000)) {
-
+        for (i in 1..15) {
+            for (value in listOf(-1, 10000)) {
+                assertThrows<IllegalArgumentException> {
+                    PatternScore(
+                        if (i == 1) value else 0,
+                        if (i == 2) value else 0,
+                        if (i == 3) value else 0,
+                        if (i == 4) value else 0,
+                        if (i == 5) value else 0,
+                        if (i == 6) value else 0,
+                        if (i == 7) value else 0,
+                        if (i == 8) value else 0,
+                        if (i == 9) value else 0,
+                        if (i == 10) value else 0,
+                        if (i == 11) value else 0,
+                        if (i == 12) value else 0,
+                        if (i == 13) value else 0,
+                        if (i == 14) value else 0,
+                        if (i == 15) value else 0,
+                    )
+                }
+            }
         }
     }
 }
